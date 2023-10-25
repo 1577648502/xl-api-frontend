e1c8e9d81672a4e64cb43d14d26ead5a8a98666d
winx <1577648502@qq.com>
2023-10-25 14:15:29 +0800

    update 自动化提交和logo

--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -86,7 +86,7 @@ const Login: React.FC = () => {
             minWidth: 280,
             maxWidth: '75vw',
           }}
-          logo={<img alt="logo" src="http://110.41.132.124:9000/public/3HrmkRHc-WX20230508-175957%402x.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=minioadmin%2F20231024%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231024T080103Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=b5f43262ded4f6f47ce93d0a8d44d02d79a9a4e114f1f37bfba72bf696de76ea"/>}
+          logo={<img alt="logo" src="http://110.41.132.124:9000/public/KgKkdTOM-WX20230508-175957%402x.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=minioadmin%2F20231025%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231025T061441Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=f70760c11c10b23d348cf390577a58218446e48afc5d9af16a72e33a1369a10d"/>}
           title="Xl-API 接口开放平台"
           subTitle={'Xl-API 接口开放平台致力于提供稳定、安全、高效的接口调用服务'}
           initialValues={{
